8308ea70ac1ffc1e7e8c2c3d55ec83f6b6eb7409
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-06-13 08:36:39 -0300

    .

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,9 +3,14 @@ import mongoose from "mongoose";
 
 const user = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
+const host = process.env.MONGO_HOST;
+const port = process.env.MONGO_PORT;
+const source = process.env.MONGO_AUTH_SOURCE;
 
 mongoose
-  .connect(`mongodb://${user}:${password}@localhost:27017/user_session?authSource=admin`)
+  .connect(
+    `mongodb://${user}:${password}@${host}:${port}/user_session?authSource=${source}`
+  )
   .then(() => {
     console.log("MongoDB connected!");
   })
