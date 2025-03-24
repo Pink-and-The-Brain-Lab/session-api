e5ca32d5f8932abc5a4976beedfc1282c5780630
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-03-24 07:55:28 -0300

    jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: 'ts-jest',
+  testMatch: ['**/src/**/*.spec.ts', '**/?(*.)+(spec|test).ts'],
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  resetMocks: false,
+  collectCoverage: true
+};
